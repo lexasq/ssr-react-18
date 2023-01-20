b76e1e0a5f845b72a21988b7f3f5715272c0466c
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2023-01-20 17:50:53 +0200

    Initial commit

---
+++ host/next.config.js
@@ -0,0 +1,21 @@
+// host/next.config.js
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+module.exports = {
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+          new NextFederationPlugin({
+            name: 'host',
+            remotes: {
+              remote: 'remote@http://localhost:3001/remote.js',
+            },
+            filename: 'static/chunks/remoteEntry.js',
+          }),
+      );
+    }
+
+    return config;
+  },
+};
+
---
+++ remote/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            This is my remote nav
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;
